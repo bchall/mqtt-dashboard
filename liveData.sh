2969d108fe6ad56df5a1bab7cfe8374d52130012
Brandon Hall <brandon.hall@arrow.com>
2019-03-13 14:18:39 -0600

    Update liveData.sh

--- liveData.sh
+++ liveData.sh
@@ -8,6 +8,6 @@ mem_usage=`free -m | awk 'NR==2{printf "%s", $3,$2 }'`;
 disk_usage=`df -h | awk '$NF=="/"{printf "%d", $3,$2,$5}'`;
 cpu_load=`top -bn1 | grep load | awk '{printf "%.2f", $(NF-2)}'`; 
 
-        mosquitto_pub -h 127.0.0.1 -t pi/telemetry --qos 1 -m "{\"host_name\": $host_name,\"mem_usage\": $mem_usage,\"disk_usage\": $disk_usage,\"cpu_load\": $cpu_load}";
+        mosquitto_pub -h 127.0.0.1 -t pi/telemetry --qos 1 -m "{\"host_name\": \"$host_name\",\"mem_usage\": $mem_usage,\"disk_usage\": $disk_usage,\"cpu_load\": $cpu_load}";
 sleep 10
     done
